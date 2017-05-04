9134ead733dbe84a35bb8f70e77fa2afb85f6f1d
zz <zz.at.field@gmail.com>
2017-05-04 20:32:02 +0800

    Backup

--- pelicanconf.py
+++ pelicanconf.py
@@ -21,6 +21,8 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+SUMMARY_MAX_LENGTH=90
+
 # Blogroll
 # LINKS = (('Pelican', 'http://getpelican.com/'),
 #          ('Python.org', 'http://python.org/'),

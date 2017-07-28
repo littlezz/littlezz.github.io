4f832e2fe2720ed0a32709757557ebf6309b691a
zz <zz.at.field@gmail.com>
2017-07-28 16:38:16 +0800

    Backup

--- pelicanconf.py
+++ pelicanconf.py
@@ -111,6 +111,6 @@ STATUSCAKE=False
 #           ('github', 'https://github.com/alexandrevicenzi'),)
 # ADD_THIS_ID = 'ra-77hh6723hhjd'
 # DISQUS_SITENAME = 'yoursite'
-# GOOGLE_ANALYTICS = 'UA-102694300-1'
+GOOGLE_ANALYTICS = 'UA-102694300-1'
 # GOOGLE_TAG_MANAGER = 'GTM-ABCDEF'
 # STATUSCAKE = { 'trackid': 'your-id', 'days': 7 }
